cf8c76adea7089d6834201603bb559b76a715a3a
Mustafa Mustafa <mstftsm@gmail.com>
2020-04-17 00:20:42 -0700

    Change NERSC images tag

--- pytorch/build_image.sh
+++ pytorch/build_image.sh
@@ -1,8 +1,8 @@
-export NVC_TAG=20.03-py3
-export NERSC_TAG=1.5.0_v0
+NVC_TAG=20.03
+NERSC_TAG="ngc_${NVC_TAG}_v0"
 
 if [ ! -d nersc-tensorboard-helper ]; then
   git clone git@github.com:NERSC/nersc-tensorboard-helper.git
 fi
 
-docker build --build-arg nvc_tag=$NVC_TAG -t nersc/pytorch:$NERSC_TAG -f Dockerfile .
+docker build --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/pytorch:$NERSC_TAG -f Dockerfile .
