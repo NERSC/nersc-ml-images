06f72846a501f73cb568086ced031f8afb0b7347
Mustafa Mustafa <mstftsm@gmail.com>
2020-04-17 15:21:01 -0700

    Add TF dockerfiles

---
+++ tensorflow/build_image.sh
@@ -0,0 +1,8 @@
+NVC_TAG=20.03-tf1
+NERSC_TAG="ngc-${NVC_TAG}-v0"
+
+if [ ! -d nersc-tensorboard-helper ]; then
+  git clone git@github.com:NERSC/nersc-tensorboard-helper.git
+fi
+
+docker build --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/tensorflow:$NERSC_TAG -f Dockerfile .
