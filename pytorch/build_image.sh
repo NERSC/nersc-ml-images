524e8a6ce381afb5bcd5f48108ba5390412be7a8
Steve Farrell <sparticlesteve@gmail.com>
2021-04-20 11:31:57 -0700

    Update image scripts
    Update installation method of NERSC TB helper.
    
    Some dockerfile cleanup
    
    Update TF version to latest.

--- pytorch/build_image.sh
+++ pytorch/build_image.sh
@@ -1,8 +1,4 @@
 NVC_TAG=21.02
 NERSC_TAG="ngc-${NVC_TAG}-v0"
 
-if [ ! -d nersc-tensorboard-helper ]; then
-  git clone git@github.com:NERSC/nersc-tensorboard-helper.git
-fi
-
 docker build --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/pytorch:$NERSC_TAG -f Dockerfile .
--- tensorflow/build_image.sh
+++ tensorflow/build_image.sh
@@ -1,8 +1,4 @@
-NVC_TAG=20.08-tf1
+NVC_TAG=21.03-tf2
 NERSC_TAG="ngc-${NVC_TAG}-v0"
-
-if [ ! -d nersc-tensorboard-helper ]; then
-  git clone git@github.com:NERSC/nersc-tensorboard-helper.git
-fi
 
 docker build --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/tensorflow:$NERSC_TAG -f Dockerfile .
