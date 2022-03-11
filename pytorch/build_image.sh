cfb47cdbf567f57f587032358e8bf330e1fc91d9
Steve Farrell <sparticlesteve@gmail.com>
2022-03-11 11:02:44 -0800

    update pytorch to 22.02

--- pytorch/build_image.sh
+++ pytorch/build_image.sh
@@ -1,4 +1,4 @@
-NVC_TAG=21.08
-NERSC_TAG="ngc-${NVC_TAG}-v2"
+NVC_TAG=22.02
+NERSC_TAG="ngc-${NVC_TAG}-v0"
 
 docker build --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/pytorch:$NERSC_TAG -f Dockerfile .
