79e179a44e79ddd1bec9f7cf434e9c1a9a19efb5
Steve Farrell <sparticlesteve@gmail.com>
2022-04-28 17:21:21 -0700

    update tensorflow to 22.04-tf2

--- tensorflow/build_image.sh
+++ tensorflow/build_image.sh
@@ -1,4 +1,4 @@
-NVC_TAG=21.08-tf2
+NVC_TAG=22.04-tf2
 NERSC_TAG="ngc-${NVC_TAG}-v1"
 
 docker build --platform linux/amd64 --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/tensorflow:$NERSC_TAG -f Dockerfile .
