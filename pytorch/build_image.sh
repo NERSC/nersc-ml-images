8b9ec14d93a23e6eb83ce148bc82c039364d6d9a
Steve Farrell <sparticlesteve@gmail.com>
2023-05-03 23:18:36 -0700

    update images to 23.04

--- pytorch/build_image.sh
+++ pytorch/build_image.sh
@@ -1,4 +1,4 @@
-NVC_TAG=22.09
+NVC_TAG=23.04
 NERSC_TAG="ngc-${NVC_TAG}-v0"
 
 docker build --platform linux/amd64 --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/pytorch:$NERSC_TAG -f Dockerfile .
--- tensorflow/build_image.sh
+++ tensorflow/build_image.sh
@@ -1,4 +1,4 @@
-NVC_TAG=22.09-tf2
+NVC_TAG=23.04-tf2
 NERSC_TAG="ngc-${NVC_TAG}-v0"
 
 docker build --platform linux/amd64 --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/tensorflow:$NERSC_TAG -f Dockerfile .
