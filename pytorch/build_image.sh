c89a589bba6d65a990c3e0416afac7d3ecf7ab42
Steve Farrell <sparticlesteve@gmail.com>
2024-01-24 11:32:44 -0800

    PyG and ngc 23.12

--- pytorch/build_image.sh
+++ pytorch/build_image.sh
@@ -1,4 +1,4 @@
-NVC_TAG=23.07
-NERSC_TAG="ngc-${NVC_TAG}-v1"
+NVC_TAG=23.12
+NERSC_TAG="ngc-${NVC_TAG}-v0"
 
 docker build --platform linux/amd64 --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/pytorch:$NERSC_TAG -f Dockerfile .
